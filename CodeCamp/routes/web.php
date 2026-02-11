735b58bea7a89393dd4a676479e80b67c14ad4b8
kang-76 <25142019@imail.sunway.edu.my>
2026-02-11 16:56:13 +0800

    Update web.php
    Matching the routes

--- CodeCamp/routes/web.php
+++ CodeCamp/routes/web.php
@@ -1,27 +1,35 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\ForgotPasswordController;
 
-// This loads your index page
 Route::get('/', function () {
     return view('index.index'); 
 });
 
-Route::post('/login', function () {
-    return "Login logic goes here later.";
-})->name('login');
+Route::post('/login', [RegisterController::class, 'login'])->name('login');
 
 Route::post('/logout', function () {
-    return "Logout logic goes here later.";
+    Auth::logout();
+    request()->session()->invalidate();
+    request()->session()->regenerateToken();
+    return redirect('/');
 })->name('logout');
 
-Route::post('/register', function () {
-    return "Registration logic goes here later.";
-})->name('register');
+Route::post('/register', [RegisterController::class, 'register'])->name('register');
 
 Route::get('/forgot-password', function () {
     return view('forgotPassword.forgotPassword'); 
 })->name('password.request');
+
+Route::post('/forgot-password', [ForgotPasswordController::class, 'sendResetCode'])->name('password.email');
+
+Route::get('/verify-password/{id}', function ($id) {
+    return view('auth.verifyPassword', ['id' => $id]);
+})->name('password.verify');
+
+Route::post('/reset-password', [ForgotPasswordController::class, 'resetPassword'])->name('password.update');
 
 Route::get('/about', function () { return view('about.aboutUs'); })->name('about');
 
